65359d7ca194328d2d3e24a151fe76db32596f49
garizs <payka04@hotmail.com>
2020-07-14 22:14:38 +0400

    release

--- main.py
+++ main.py
@@ -4,24 +4,30 @@ from youtube import Youtube
 from aiogram import Bot, Dispatcher, types, executor
 from database import DataBase
 import json
+import video
 
 db = DataBase('database.db')
 bot = Bot(config.TGtoken)
 disp = Dispatcher(bot)
-commands_list = {'/help', '/start', '/test'}
+commands_list = {'/help', '/start'}
 channel_id = 'UCWxQrOSmSwCp8MRAWe2pJ-g'
 
 
 @disp.message_handler(commands=['start'])
 async def start(message):
     await bot.send_message(message.chat.id,
-                           "Добро пожаловать, {0.first_name}!\n Чтобы получить список доступных команд введите /help".format(message.from_user, await bot.get_me()),
+                           "Добро пожаловать, {0.first_name}!\n Чтобы получить список доступных команд введите /help".format(
+                               message.from_user, await bot.get_me()),
                            parse_mode='html')
 
 
 @disp.message_handler(commands=['help'])
 async def help(message):
-    await bot.send_message(message.chat.id, 'Доступные команды:\n• /help — список доступных команд\n• /subscribe — подписаться на рассылку \n• /unsubscribe — отписаться от рассылки')
+    await bot.send_message(message.chat.id,
+                           'Доступные команды:\n'
+                           '• /help — список доступных команд\n'
+                           '• /subscribe — подписаться на рассылку \n'
+                           '• /unsubscribe — отписаться от рассылки \n')
 
 
 @disp.message_handler(commands=['subscribe'])
@@ -44,7 +50,8 @@ async def unsubscribe(message):
 
 @disp.message_handler(lambda message: message.text not in commands_list and message.text[0] == '/')
 async def error_allert(message: types.Message):
-    await message.answer("Вы ввели неверную команду.\nВоспользуйтесь /help для получения списка доступных команд.")
+    await message.answer("Вы ввели неверную команду.\n"
+                         "Воспользуйтесь /help для получения списка доступных команд.")
 
 
 async def scheduled(wait_for):
@@ -56,14 +63,15 @@ async def scheduled(wait_for):
 
         video_links1 = Youtube.get_all_video_in_channel(channel_id)
         subscriptions = db.get_subscriptions()
+
         if video_links1[0] != video_links[0]:
             for s in subscriptions:
-                await bot.send_message(s[1], video_links1[0], disable_notification=True)
+                vd = video.vid_info(video_links1[0])
+                await bot.send_message(s[1], vd['title'] + "\n" + video_links1[0],
+                                       disable_notification=True)
         video_links = video_links1
 
 
 if __name__ == '__main__':
-    disp.loop.create_task(scheduled(120))
+    disp.loop.create_task(scheduled(10))
     executor.start_polling(disp, skip_updates=True)
-
-
--- youtube.py
+++ youtube.py
@@ -1,14 +1,7 @@
-import re
-import os.path
-import requests
-from bs4 import BeautifulSoup as bs
-from urllib.parse import urlparse
-import config
-import google_auth_oauthlib.flow
-import googleapiclient.discovery
-import googleapiclient.errors
-import urllib
 import json
+import urllib
+
+import config
 
 
 class Youtube:
@@ -17,20 +10,14 @@ class Youtube:
         base_video_url = 'https://www.youtube.com/watch?v='
         base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
         first_url = base_search_url + 'key={}&channelId={}&part=snippet,id&order=date&maxResults=25'.format(api_key,
-                                                                                                            channel_id)
+                                                                                                          channel_id)
         video_links = []
         url = first_url
-        k = 0
-        bool = True
-        while bool:
-            with urllib.request.urlopen(url) as inp:
-                resp = json.load(inp)
-            for i in resp['items']:
-                if i['id']['kind'] == "youtube#video":
-                    video_links.append(base_video_url + i['id']['videoId'])
-                    k += 1
-                    if k == 3:
-                        bool = False
+        with urllib.request.urlopen(url) as inp:
+            resp = json.load(inp)
+        for i in resp['items']:
+            if i['id']['kind'] == "youtube#video":
+                video_links.append(base_video_url + i['id']['videoId'])
         data = video_links
         with open('data.json', 'w') as file:
             json.dump(data, file)
